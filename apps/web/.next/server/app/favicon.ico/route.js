a26875e7c62f5b93ad0b923cae4baeb3b4adf019
Pascal Watteel <pascal@watteel.com>
2024-12-05 11:15:31 +0400

    chore: update gitignore and remove tracked cache files

--- apps/web/.next/server/app/favicon.ico/route.js
+++
@@ -1,5 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/favicon.ico/route.js";
-const runtime = require("../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/aab00_next_5924ae._.js");
-runtime.loadChunk("server/chunks/[root of the server]__3c74a6._.js");
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.0.3_react-dom@19.0.0-rc-66855b96-20241106_react@19.0.0-rc-66855b96-20241106__react@19.0.0-rc-66855b96-20241106/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/apps/web/src/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- apps/web/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CtjGwDztb8T1KMYi1mEEDFryGDL+2JUtiAUbWvb40bY=\"\n}"
